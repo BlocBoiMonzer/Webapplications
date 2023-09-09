7eee0fbfacec5120c0bf6aac0d4cf85b6f48a9ab
BlocBoiMonzer <77080735+BlocBoiMonzer@users.noreply.github.com>
2023-09-10 00:20:37 +0200

    work

---
+++ Lecture1/live01.js
@@ -0,0 +1,8 @@
+const li = document.querySelectorAll("li");
+const ul = document.querySelector("ul");
+const add = document.getElementById("add");
+const sort = document.getElementById("sort");
+
+function random(){
+    return Math.floor(Math.random() * 9 + 1);
+  };
